f0275d880d546f81b2698f0111a4019488e5226f
oussamaaiachi89-blip <oussamaaiachi89@gmail.com>
2026-07-07 22:26:46 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,20 @@
-import time
+import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 
-while True:
-    print("البوت يعمل ويراقب الأهداف...")
-    time.sleep(60)
+
+TOKEN = "8935013119:AAG7SSGEmpJV5AoADxJcMDHuJL3mQrvpQeo"
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="أهلاً! البوت يعمل الآن ويراقب الأهداف.")
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token(TOKEN).build()
+    
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
+    
+    print("البوت يعمل الآن...")
+    application.run_polling()
+    
   
